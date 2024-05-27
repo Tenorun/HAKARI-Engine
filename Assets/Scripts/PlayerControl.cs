1e44cfd44b42f3b967a6761a56b0d6172fe76c47
Tenorun <ngdtv123@gmail.com>
2024-05-28 03:30:57 +0900

    Player Move Animation

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-
     public float moveSpeed; // 플레이어 움직임 속도
     const float DEFAULT_MOVE_SPEED = 6f;    //걷기 속도
     const float SPRINT_MOVE_SPEED = 13f;    //달리기 속도
@@ -12,7 +11,9 @@ public class PlayerControl : MonoBehaviour
     private Rigidbody2D rb; // Reference to the Rigidbody2D component
     private Vector2 movement; // Variable to store movement direction
 
-    public float stareAngle;    //플레이어가 바라보는 각도(일반적으로 컨트롤의 방향의 각도다.)
+
+    private Animator anim;
+    public float stareAngle = -90f;    //플레이어가 바라보는 각도(일반적으로 컨트롤의 방향의 각도다.)
     public bool isMoving;       //플레이어 움직임 여부
 
     public bool LockPlayerControl = false;  //플레이어 컨트롤 잠금 여부
@@ -37,12 +38,17 @@ public class PlayerControl : MonoBehaviour
     //플레이어 입력에 따라 애니메이션 업데이트
     void UpdateAnimation_PlayerControl()
     {
-
+        anim.SetBool("is Moving", isMoving);
+        anim.SetFloat("Stare Angle", stareAngle);
     }
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component
+        rb = GetComponent<Rigidbody2D>();   //Rigidbody2D 컴포넌트 부르기
+        anim = GetComponent<Animator>();    //Animator 컴포넌트 부르기
+
+        stareAngle = -90f;
+        isMoving = false;
         LockPlayerControl = false;
     }
 
@@ -55,7 +61,7 @@ public class PlayerControl : MonoBehaviour
             movement.y = Input.GetAxisRaw("Vertical"); // Get vertical input
 
             // 입력여부에 따라 isMoving을 참 거짓으로 설정
-            if(movement.x == 0f && movement.y == 0f) isMoving = false;
+            if (movement.x == 0f && movement.y == 0f) isMoving = false;
             else isMoving = true;
 
             // Normalize movement vector to ensure consistent speed in all directions

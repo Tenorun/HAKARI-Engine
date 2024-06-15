6fd13b1b7a5a14b570e5d807331e23290b34a916
Tenorun <ngdtv123@gmail.com>
2024-06-15 12:50:35 +0900

    초상화 데이터 관리 기능 구현 2

--- Assets/Scripts/PortraitManager.cs
+++ Assets/Scripts/PortraitManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -5,31 +6,77 @@ using UnityEngine;
 
 public class PortraitManager : MonoBehaviour
 {
+    //초상화 메인 폴더 상대경로
     public string relativeFolderPath = string.Empty;
-    public Sprite[,] imageArray = new Sprite[10000, 100];
-
-
+    //초상화 메인 폴더 절대경로
     [SerializeField] private string mainFolderPath;
 
-    private string[] characterFolderNames = new string[100];
-    private int[] folderPathNums = new int[100];
 
-    
+    //폴더 이름 앞 번호로 배열의 위치에 할당된 폴더 이름들 
+    [SerializeField] private string[] charFolderNames;
+    //이미지 배열
+    public Sprite[,] imageArray;
+
+
+    //캐릭터 폴더 룩업 할당하기
     void LoadFolders()
     {
-        mainFolderPath = Path.Combine(Application.dataPath, relativeFolderPath); // 상대 경로를 절대 경로로 변환
-        characterFolderNames = Directory.GetFiles(mainFolderPath); //캐릭터 폴더 이름 
+        string[] charFolderNamesTemp;
 
-        for(int i = 0; i < characterFolderNames.Length; i++)
+        mainFolderPath = Path.Combine(Application.dataPath, relativeFolderPath); // 상대 경로를 절대경로로 변환
+        charFolderNamesTemp = Directory.GetFiles(mainFolderPath); //캐릭터 폴더 이름 
+
+        //캐릭터 이미지 배열, 캐릭터 폴더 이름 룩업의 길이를 마지막 초상화 ID의 크기만큼 할당
+        //폴더의 마지막 초상화 ID를 구하기
+        string[] lastSplit = Path.GetFileNameWithoutExtension(charFolderNamesTemp[charFolderNamesTemp.Length - 1]).Split('-');
+        int lastPortraitID;
+        //초상화 ID만큼 크기 할당하기 (만약 마지막 초상화 ID에 할당할 숫자를 찾을 수 없을 시 10000크기로 할당)
+        if (int.TryParse(lastSplit[0], out lastPortraitID))
         {
-            string folderName = Path.GetFileNameWithoutExtension(characterFolderNames[i]);
+            charFolderNames = new string[lastPortraitID + 1];
+            imageArray = new Sprite[lastPortraitID + 1, 100];
+        }
+        else
+        {
+            Debug.LogWarning($"초상화 폴더의 마지막 이름, \"{charFolderNamesTemp[charFolderNamesTemp.Length - 1]}\"이 올바르지 않은 형식 이므로 이미지 배열, 폴더 이름 배열의 크기가 10000만큼 할당되었습니다.");
+            charFolderNames = new string[10000];
+            imageArray = new Sprite[10000, 100];
+        }
+
+
+        //캐릭터 폴더 이름 룩업에 폴더 이름 할당하기
+        for (int i = 0; i < charFolderNamesTemp.Length; i++)
+        {
+            //캐릭터 ID 구하고 할당하기
+            string folderName = Path.GetFileNameWithoutExtension(charFolderNamesTemp[i]);
             string[] splitName = folderName.Split('-');
 
             int charPortraitID;
 
             if (int.TryParse(splitName[0], out charPortraitID))
             {
-                characterFolderNames[charPortraitID] = folderName;
+                charFolderNames[charPortraitID] = folderName;
+            }
+            else
+            {
+                Debug.LogWarning($"초상화 폴더의 이름,{folderName} 이 올바르지 않은 형식 이므로, 해당 폴더 이름을 배열에 할당 할 수 없습니다.");
+            }
+        }
+    }
+
+    void LoadImages()
+    {
+        for(int i = 0; i < charFolderNames.Length; i++)
+        {
+            if (charFolderNames[i] != string.Empty)
+            {
+                //TODO: charFolderNameLookUps에서 폴더 이름을 가져와서 mainFolderPath와 합쳐 하려는 imageArray에 할당 하려는 캐릭터의 폴더 경로를 얻은 후,
+                //폴더 내 이미지들을 가져와서 imageArray에 imageArray[캐릭터 번호][표정 번호]위치에 할당하는 스크립트 작성
+                string path = mainFolderPath + '\\' + charFolderNames[i];
+
+                string[] imagePaths = Directory.GetFiles(path, "*.png"); // 폴더 내의 모든 PNG 파일 이름 가져오기
+
+                //할당 위치 구하기
             }
         }
     }
@@ -44,13 +91,8 @@ public class PortraitManager : MonoBehaviour
 
     void Start()
     {
-        for(int i = 0; i < characterFolderNames.Length; i++)
-        {
-            characterFolderNames[i] = "(none)";
-            folderPathNums[i] = -1;
-        }
-
         LoadFolders();
+        LoadImages();
     }
 
     // Update is called once per frame
